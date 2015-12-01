e3d8ae40ba7c42f9d86e2e729dedab1ae1769c49
ACUVE <sub_chon@yahoo.co.jp>
2015-12-01 17:14:20 +0900

    Add crc8 and crc16

--- hash.cpp
+++ hash.cpp
@@ -1,6 +1,8 @@
 #include <cstdint>
 #include "hash.hpp"
 
+namespace hash{
+
 template< typename T, std::size_t size >
 struct array
 {
@@ -15,19 +17,79 @@ struct array
     }
 };
 
-constexpr auto calc_crc8_table()
+constexpr auto calc_crc8_table() noexcept
 {
     array< std::uint8_t, 256 > buff = {};
-    for( std::uint8_t i = 0; i < 256; ++i )
+    for( int i = 0; i < 256; ++i )
     {
+        std::uint8_t v = i;
         std::uint8_t c = 0;
-        for( std::uint8_t j = 0; j < 8; ++j )
+        for( int j = 0; j < 8; ++j )
         {
-            c = (c & 1) ? 0x07 ^ (c >> 1) : c >> 1;
+            if( v & (1 << (7 - j)) )
+            {
+                c ^= 0b00000111 << (7 - j);
+                if( j < 7 )
+                    v ^= 0b00000111 >> (j + 1);
+            }
         }
         buff[ i ] = c;
     }
     return buff;
 }
 
-// constexpr auto crc8_table = calc_crc8_table();
+constexpr auto crc8_table = calc_crc8_table();
+void crc8_update( std::uint8_t &crc, std::uint8_t const data ) noexcept
+{
+    crc = crc8_table[ crc ^ data ];
+}
+void crc8_update( std::uint8_t &crc, std::uint8_t const *data, std::size_t const len ) noexcept
+{
+    for( std::size_t i = 0; i < len; ++i )
+        crc8_update( crc, data[ i ] );
+}
+std::uint8_t crc8( std::uint8_t const *data, std::size_t const len ) noexcept
+{
+    std::uint8_t crc = 0;
+    crc8_update( crc, data, len );
+    return crc;
+}
+
+constexpr auto calc_crc16_table() noexcept
+{
+    array< std::uint16_t, 256 > buff = {};
+    for( int i = 0; i < 256; ++i )
+    {
+        std::uint8_t v = i;
+        std::uint16_t c = 0;
+        for( int j = 0; j < 8; ++j )
+        {
+            if( v & (1 << (7 - j)) )
+            {
+                c ^= 0b1000000000000101 << (7 - j);
+                if( j < 7 )
+                    v ^= 0b1000000000000101 >> (j + 9);
+            }
+        }
+        buff[ i ] = c;
+    }
+    return buff;
+}
+constexpr auto crc16_table = calc_crc16_table();
+void crc16_update( std::uint16_t &crc, std::uint8_t const data ) noexcept
+{
+    crc = ((crc << 8) ^ crc16_table[ (crc >> 8) ^ data ]);
+}
+void crc16_update( std::uint16_t &crc, std::uint8_t const *data, std::size_t const len ) noexcept
+{
+    for( std::size_t i = 0; i < len; ++i )
+        crc16_update( crc, data[ i ] );
+}
+std::uint16_t crc16( std::uint8_t const *data, std::size_t const len ) noexcept
+{
+    std::uint16_t crc = 0;
+    crc16_update( crc, data, len );
+    return crc;
+}
+
+} // namespace hash
--- hash.hpp
+++ hash.hpp
@@ -1,8 +1,19 @@
 #ifndef HASH_HPP
 #define HASH_HPP
 
+#include <cstddef>
 #include <cstdint>
 
+namespace hash{
 
+void crc8_update( std::uint8_t &crc, std::uint8_t data ) noexcept;;
+void crc8_update( std::uint8_t &crc, std::uint8_t const *data, std::size_t len ) noexcept;
+std::uint8_t crc8( std::uint8_t const *data, std::size_t len ) noexcept;
+
+void crc16_update( std::uint16_t &crc, std::uint8_t data ) noexcept;
+void crc16_update( std::uint16_t &crc, std::uint8_t const *data, std::size_t len ) noexcept;
+std::uint16_t crc16( std::uint8_t const *data, std::size_t len ) noexcept;
+
+} // namespace hash
 
 #endif // HASH_HPP
