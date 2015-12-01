89d7c1f36996071260b836e4e4ab40e50ca15fa2
ACUVE <sub_chon@yahoo.co.jp>
2015-12-01 18:19:25 +0900

    Use args to determine filename to open

--- main.cpp
+++ main.cpp
@@ -39,38 +39,41 @@ bool fatal( Args &&... args )
 
 int main( int argc, char **argv )
 {
-    std::ifstream file( filename, std::ios::binary | std::ios::ate );
+    char const *openfilename = argc > 1 ? argv[ 1 ] : filename;
+    std::ifstream file( openfilename, std::ios::binary | std::ios::ate );
+    if( !file )
+        fatal( openfilename, " open error" );
     std::streamsize size = file.tellg();
     file.seekg( 0, std::ios::beg );
     auto buff = std::make_unique< std::uint8_t[] >(size);
     if( !file.read( (char *)buff.get(), size ) )
-        fatal( filename, " is not readable." );
+        fatal( openfilename, " is not readable." );
     bitstream bs( std::move( buff ), size );
     if( std::memcmp( bs.get_bytes< 4 >().data(), FLAC::STREAM_SYNC_STRING, 4 ) != 0 )
-        fatal( filename, " is not FLAC file." );
+        fatal( openfilename, " is not FLAC file." );
     if( bs.is_error() )
-        fatal( filename, " is not FLAC file?" );
+        fatal( openfilename, " is not FLAC file?" );
     
     FLAC::optional< FLAC::MetaData::StreamInfo > si;
     while( true )
     {
         auto md = FLAC::ReadMetadata( bs );
         if( !md )
-            fatal( filename, ": ReadMetadata error." );
+            fatal( openfilename, ": ReadMetadata error." );
         if( md->type == FLAC::MetaData::Type::STREAMINFO )
             si = md->data.data< FLAC::MetaData::StreamInfo >();
         if( md->is_last )
             break;
     }
     if( !si )
-        fatal( filename, ": No StreamInfo.");
+        fatal( openfilename, ": No StreamInfo.");
     FLAC::PrintStreamInfo( *si );
     
     while( true )
     {
         auto frame = FLAC::ReadFrame( bs, *si );
         if( !frame )
-            fatal( filename, ": ReadFrame error.");
+            fatal( openfilename, ": ReadFrame error.");
         std::cout << "-----------------------------------" << std::endl;
         FLAC::PrintFrame( *frame );
         if( std::get< 0 >( bs.get_position() ) >= size )
